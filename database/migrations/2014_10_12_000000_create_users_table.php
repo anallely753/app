ba0c6da901be33f18babe2f0dc5850a454cc8bde
erick <rodreri@gmail.com>
2021-05-22 17:35:20 -0500

    Creacion de tablas V1

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,11 +16,16 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('fisrtLastName');
+            $table->string('secondLastName');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->boolean('admin')->default(false);
+            $table->string('avatar')->nullable();
+
         });
     }
 
